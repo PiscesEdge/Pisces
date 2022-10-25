aa87fa8cf042f5a7f497a152a3321fe7a74082b3
Vida Majd <piscesedge@gmail.com>
2022-10-25 06:37:56 -0400

    more changes

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
-app.set('views', path.join(dirname, 'views'));
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = ('morgan');
+
+let indexRouter = require('./routes/index');
+let usersRouters = require('./routes/users');
+
+let app = express();
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use()
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+app.use(function (err, reg, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(err.status || 500);
+    res.remder(error, { title: 'Error', });
+});
+
+module.exports = app;
---
+++ routes/index.routes.js
@@ -0,0 +1,37 @@
+let express = require('express');
+let router = express.Router();
+
+let indexController = require('../controllers/index.controller');
+
+/* GET pages */
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Home', });
+});
+
+
+router.get('/home', function (req, res, next) {
+    res.render('index', { title: 'Home', });
+});
+
+
+router.get('/about', function (req, res, next) {
+    res.render('index', { title: 'About', });
+});
+
+
+router.get('/projects', function (req, res, next) {
+    res.render('index', { title: 'Projects', });
+});
+
+
+router.get('/services', function (req, res, next) {
+    res.render('index', { title: 'Services', });
+});
+
+
+router.get('/contact', function (req, res, next) {
+    res.render('index', { title: 'Contact', });
+});
+
+
+module.exports = router;
